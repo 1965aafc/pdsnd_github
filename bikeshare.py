81fa3dc41cc5f70d05b216c806e0412618bab93a
Alvaro Fuentes Coiana <1965aafc@gmail.com>
2019-04-14 21:33:54 -0400

    A print statement eliminated

--- bikeshare.py
+++ bikeshare.py
@@ -15,8 +15,7 @@ print('Specify a city, month and day separated by commas')
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
-    print('Specify a city, month and day separated by commas')
+    print('Hello! Let\'s explore some US bikeshare data! \n Specify a city, month and day separated by commas')
 
     cities = ['chicago', 'new york city', 'washington']
     months = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
@@ -25,7 +24,7 @@ print('Specify a city, month and day separated by commas')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input('Specify a city: ').lower()
     while city not in cities:
-        city = input('city is not in our database. You can select between {chicago, new york city or washington.\n Specify a city: ').lower() 
+        city = input('city is not in our database. You can select between {chicago, new york city or washington.\n Specify a city: ').lower()
 
 
     # get user input for month (all, january, february, ... , june)
